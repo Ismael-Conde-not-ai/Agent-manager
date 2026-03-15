b500aa791cef43b03339fa5818131eb7e8f200bf
Ismael Conde <condegomezismaelnicolas@gmail.com>
2026-03-15 17:12:48 -0400

    System monitoring endpoints added

--- api/server.py
+++ api/server.py
@@ -1,3 +1,5 @@
+from os import name
+
 from fastapi import FastAPI
 from agents.agent import AIagent
 from core.agentManager import aIagentManager
@@ -59,3 +61,37 @@ def agent_status():
         "status":agent.status,
         "memory":agent.memory
     }
+
+@app.get("/system/health")
+def system_health():
+    '''It tells if the system is operational'''
+    return{
+        "status":"running",
+        "agent_loaded": agent is not None
+    }
+
+@app.get("/system/metrics")
+def system_metrics ():
+    '''Observability of agent metrics'''
+    if not agent:
+        return{"error":"Agent not initialized"}
+    
+    return{
+        "agent_name":agent.name,
+        "energy": agent.energy,
+        "status":agent.status,
+        "memory_entries":len(agent.memory),
+        "remaining_plan_steps": len(agent.plan)
+    }
+
+@app.get("/system/logs")
+def get_logs():
+    '''Returns last 20 log entries, if not return error log'''
+    try:
+        with open("logs/agent.log","r") as file:
+            logs = file.readlines()
+
+        return {"logs":logs[-20:]}
+    
+    except FileNotFoundError:
+        return {"error","Log file not found"}
